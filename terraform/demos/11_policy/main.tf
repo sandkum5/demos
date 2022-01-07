913d2d6d55d29b3001496138a487a81d003493c8
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-01-06 23:12:14 -0800

    Create main.tf

---
+++ terraform/demos/11_policy/main.tf
@@ -0,0 +1,18 @@
+resource "intersight_ntp_policy" "ntp_policy" {
+  name        = var.Name
+  description = var.Description
+  organization {
+    object_type = "organization.Organization"
+    selector    = "$filter=Name eq '${var.Organization.Name}'"
+  }
+  enabled     = var.Enabled
+  ntp_servers = var.NtpServers
+  timezone    = var.Timezone
+  dynamic "tags" {
+    for_each = var.Tags
+    content {
+      key   = tags.value.Key
+      value = tags.value.Value
+    }
+  }
+}
