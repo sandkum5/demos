d37ccb2b6639c93ecaf536ff3151980a2bf36c23
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-01-06 23:12:58 -0800

    Create provider.tf

---
+++ terraform/demos/11_policy/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    intersight = {
+      source  = "CiscoDevNet/intersight"
+    }
+  }
+}
+
+provider "intersight" {
+  apikey    = file("../ApiKey.txt")
+  secretkey = "../SecretKey.txt"
+  endpoint  = "https://intersight.com"
+}
