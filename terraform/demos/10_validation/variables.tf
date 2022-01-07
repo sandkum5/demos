898237f2e37eaa725929e0dd50d2d8029db16f57
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-01-06 17:18:41 -0800

    Update variables.tf

--- terraform/demos/10_validation/variables.tf
+++ terraform/demos/10_validation/variables.tf
@@ -5,8 +5,12 @@ variable "Name" {
     error_message = "Name should start with \"Demo-\"."
   }
   validation {
-    condition     = length(var.Name) > 8
-    error_message = "Minimum length should be > 8."
+    condition     = length(var.Name) > 1 && length(var.Name) <= 64
+    error_message = "The name must be between 1 and 64 characters."
+  }
+  validation {
+    condition = regex("[0-9A-Za-z\\-_\\.]+", var.Name) == var.Name
+    error_message = "The number should include alphanumeric characters, allowing special characters '-' '_' '.' ."
   }
 }
 
@@ -14,7 +18,7 @@ variable "Description" {
   type = string
   validation {
     condition = length(var.Description) > 20
-    error_message = "Please add Description for the variable."
+    error_message = "Please add Description for the variable. The letters should be <= 1024."
   }
 }
 
@@ -24,11 +28,17 @@ variable "NtpServers" {
   type = list(string)
   validation {
     condition = length(var.NtpServers) > 0 && length(var.NtpServers) <= 4
-    error_message = "NTP Servers must be between 1 to 4.
+    error_message = "NTP Servers must be between 1 to 4."
   }
 }
 
-variable "Timezone" {}
+variable "Timezone" {
+  type = string
+  validation {
+    condition = var.Timezone == "America/Los_Angeles" || var.Timezone == "America/New_York" || var.Timezone == "America/Denver" || var.Timezone == "America/Phoenix"
+    error_message = "Timezone should be one of the following: America/Los_Angeles | America/New_York | America/Denver | America/Phoenix."
+  }
+}
 
 variable "Organization" {
   type = object({ Name = string })
@@ -38,4 +48,4 @@ variable "Organization" {
 variable "Tags" {
   type = list(object({ Key = string, Value = string }))
   # type = list(map(string))
-  }
+}
